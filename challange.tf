0c6891fb23c0c33523b02fb5f19eca19d1f5d5df
Sakshi-Gawade-05 <152467969+Sakshi-Gawade-05@users.noreply.github.com>
2024-02-18 12:27:03 +0530

    updated

--- challange.tf
+++ challange.tf
@@ -1,1 +1,6 @@
+# create a file in local machine using terraform.
 
+resource"null_resource" "file" {
+ provisioner "local-exec"
+ command = "echo 'Message: ${upper("hello world!")}' >challange.txt"
+ }
