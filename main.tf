030046c0aa4aadcf506048ed33bbc17a2c66e1a7
Sakshi-Gawade-05 <152467969+Sakshi-Gawade-05@users.noreply.github.com>
2024-02-18 22:25:49 +0530

    updated

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws"{
+    region = "ap-south-1"
+    }
+
+resource "aws_instance" "demo" {
+  ami                     = "ami-0dcc1e21636832c5d"
+  instance_type           = "t2.micro"
+
+ tags = {
+    Name = "Hello world"
+  }
+}
+
